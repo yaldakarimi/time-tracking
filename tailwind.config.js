c37e8c363bce3dde3d889254013e16576e25c85a
yalda karimi <yalda.karimi88@gmail.com>
2023-10-11 18:30:43 +0200

    [npm] install needed packages

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#615D76",
+				accent: "#EBB96E",
+				darkPurple: "#262548",
+			},
+		},
+	},
+	plugins: [],
+};
